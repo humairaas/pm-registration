22bc43cb93ff147694ed04112cec641f551a2a45
Kisyok <kisoindran@gmail.com>
2021-11-15 12:50:48 +0800

    demo (in progress)

--- src/components/patient-management/patient_registration/new_registration.vue
+++ src/components/patient-management/patient_registration/new_registration.vue
@@ -18,9 +18,6 @@
               <tab-content icon="fa fa-vcard" title="2. Socio Demographic" :before-change="validateThirdTab">
                 <vue-form-generator :model="model" :schema="tabBSchema" :options="formOptions" ref="sosioDemographic" @model-updated="onModelUpdated" >
                 </vue-form-generator>
-                <div>
-                  <multiselect v-model="value" :options="options"></multiselect>
-                </div>
               </tab-content>
 
               <!-- 3rd tab: Next of Kin-->
@@ -99,6 +96,10 @@ Vue.use(IconsPlugin)
 Vue.use(VueFormGenerator)
 Vue.component('multiselect', Multiselect)
 
+// cleave.js
+require('cleave.js')
+require('cleave.js/dist/addons/cleave-phone.my')
+
 // register globally
 export default {
   components: {
@@ -107,6 +108,24 @@ export default {
     return {
       model: {
         // Demographic
+        SALUTATION: '',
+        DM_NAME: '',
+        CITIZENSHIP: '',
+        NRIC_TYPE: '',
+        NRIC_NO: '',
+        GENDER: '',
+        BIRTH_DATE: '',
+        AGE: '',
+        DM_MOBILE_NO: '',
+        DM_HOUSE_NO: '',
+        SERVICE_TYPE: '',
+        DM_ADDRESS_LINE_1: '',
+        DM_ADDRESS_LINE_2: '',
+        DM_ADDRESS_LINE_3: '',
+        DM_POSTCODE: '',
+        DM_CITY: '',
+        DM_STATE: '',
+        EXISTING_PATIENT: '',
 
         // Sosio Demographic
 
@@ -117,7 +136,255 @@ export default {
       },
       // Demographic
       tabASchema: {
-
+        fields: [
+          {
+            type: 'vueMultiSelect',
+            placeholder: 'Please select',
+            label: 'Salutation',
+            model: 'SALUTATION',
+            required: true,
+            validator: 'required',
+            selectOptions: {
+              multiple: false,
+              closeOnSelect: true,
+              maxHeight: 100,
+              showLabels: false,
+              key: 'value',
+              label: 'name',
+            },
+            styleClasses: ['col-12 col-md-4'],
+            values: [
+              { name: 'MR.', value: 1 },
+              { name: 'MRS.', value: 2 },
+            ],
+          },
+          {
+            type: 'input',
+            inputType: 'text',
+            label: 'Name (as in NRIC)',
+            model: 'DM_NAME',
+            validator: 'string',
+            required: true,
+            styleClasses: ['col-12 col-md-8'],
+          },
+          {
+            type: 'radios',
+            label: 'Citizenship',
+            model: 'CITIZENSHIP',
+            values: [
+              { value: 1, name: 'Malaysian' },
+              { value: 2, name: 'Permanent Resident' },
+              { value: 3, name: 'Foreigner' },
+            ],
+            validator: 'required',
+            // styleClasses : "col-md-7",
+          },
+          {
+            type: 'vueMultiSelect',
+            placeholder: 'Please select',
+            label: 'NRIC Type',
+            model: 'NRIC_TYPE',
+            required: true,
+            validator: 'required',
+            selectOptions: {
+              multiple: false,
+              closeOnSelect: true,
+              maxHeight: 200,
+              showLabels: false,
+              key: 'value',
+              label: 'name',
+            },
+            // styleClasses: ["col-12 col-md-6"],
+            values: [
+              { name: 'Old IC', value: 1 },
+              { name: 'New IC', value: 2 },
+              { name: 'Army', value: 3 },
+              { name: 'Police', value: 4 },
+              { name: "Mother's IC", value: 5 },
+              { name: "Father's IC", value: 6 },
+              { name: 'Work Permit', value: 7 },
+              { name: 'Passport', value: 8 },
+              { name: 'Birth Certificate', value: 9 },
+              { name: 'Pension Card', value: 10 },
+              { name: 'Others (Please Specify)', value: 11 },
+            ],
+          },
+          {
+            type: 'cleave',
+            label: 'NRIC NO',
+            model: 'NRIC_NO',
+            cleaveOptions: {
+              blocks: [6, 2, 4],
+              delimiter: '-',
+              numericOnly: true,
+            },
+            placeholder: 'XXXXXX-XX-XXXX',
+          },
+          {
+            type: 'radios',
+            label: 'Gender',
+            model: 'GENDER',
+            values: [
+              { value: 1, name: 'Male' },
+              { value: 2, name: 'Female' },
+            ],
+            validator: 'required',
+            // styleClasses : "col-md-7",
+          },
+          {
+            type: 'input',
+            inputType: 'date',
+            label: 'Date of Birth',
+            model: 'BIRTH_DATE',
+            placeholder: 'Enter Date',
+            required: true,
+            formatted: 'DD/MM/YYYY',
+            // styleClasses: 'col-md-5',
+          },
+          {
+            type: 'input',
+            inputType: 'number',
+            label: 'Age',
+            model: 'AGE',
+            validator: 'number',
+            styleClasses: 'col-md-7',
+            required: true,
+          },
+          {
+            type: 'cleave',
+            label: 'Mobile Phone Number',
+            model: 'DM_MOBILE_NO',
+            cleaveOptions: {
+              phone: true,
+              phoneRegionCode: 'MY',
+            },
+            required: true,
+            validator: 'required',
+            // styleClasses: "col-md-6"
+          },
+          {
+            type: 'cleave',
+            label: 'House Phone Number',
+            model: 'DM_HOUSE_NO',
+            cleaveOptions: {
+              phone: true,
+              phoneRegionCode: 'MY',
+            },
+            required: true,
+            validator: 'required',
+            // styleClasses: "col-md-6"
+          },
+          {
+            type: 'vueMultiSelect',
+            placeholder: 'Please select',
+            label: 'Type of Services',
+            model: 'SERVICE_TYPE',
+            required: true,
+            validator: 'required',
+            selectOptions: {
+              multiple: false,
+              closeOnSelect: true,
+              maxHeight: 200,
+              showLabels: false,
+              key: 'value',
+              label: 'name',
+            },
+            // styleClasses: ["col-12 col-md-6"],
+            values: [
+              { name: 'Consultation', value: 1 },
+              { name: 'Rehabilitation', value: 2 },
+              { name: 'Community Psychiatric Service (CPS)', value: 3 },
+            ],
+          },
+          {
+            type: 'input',
+            inputType: 'text',
+            label: 'Address',
+            model: 'DM_ADDRESS_LINE_1',
+            placeholder: 'Address line 1',
+            validator: 'string',
+          },
+          {
+            type: 'input',
+            inputType: 'text',
+            model: 'DM_ADDRESS_LINE_2',
+            placeholder: 'Address line 2',
+            validator: 'string',
+          },
+          {
+            type: 'input',
+            inputType: 'text',
+            model: 'DM_ADDRESS_LINE_3',
+            placeholder: 'Address line 3',
+            validator: 'string',
+          },
+          {
+            type: 'vueMultiSelect',
+            placeholder: 'Please select',
+            label: 'State',
+            model: 'DM_STATE',
+            required: true,
+            validator: 'required',
+            selectOptions: {
+              multiple: false,
+              closeOnSelect: true,
+              maxHeight: 200,
+              showLabels: false,
+              key: 'value',
+              label: 'name',
+            },
+            // styleClasses: ["col-12 col-md-6"],
+            values: [
+              { name: 'Selangor', value: 1 },
+              { name: 'Kuala Lumpur', value: 2 },
+              { name: 'Perak', value: 3 },
+            ],
+          },
+          {
+            type: 'vueMultiSelect',
+            placeholder: 'Please select',
+            label: 'City',
+            model: 'DM_CITY',
+            required: true,
+            validator: 'required',
+            selectOptions: {
+              multiple: false,
+              closeOnSelect: true,
+              maxHeight: 200,
+              showLabels: false,
+              key: 'value',
+              label: 'name',
+            },
+            // styleClasses: ["col-12 col-md-6"],
+            values: [
+              { name: 'Klang', value: 1 },
+              { name: 'Batu Caves', value: 2 },
+              { name: 'Subang Jaya', value: 3 },
+            ],
+          },
+          {
+            type: 'vueMultiSelect',
+            placeholder: 'Please select',
+            label: 'Postcode',
+            model: 'DM_POSTCODE',
+            required: true,
+            validator: 'required',
+            selectOptions: {
+              multiple: false,
+              closeOnSelect: true,
+              maxHeight: 200,
+              showLabels: false,
+              key: 'value',
+              label: 'name',
+            },
+            // styleClasses: ["col-12 col-md-6"],
+            values: [
+              { name: '68100', value: 1 },
+              { name: '59200', value: 2 },
+              { name: '63000', value: 3 },
+            ],
+          },
+        ],
       },
       // Sosio Demographic
       tabBSchema: {
@@ -132,7 +399,9 @@ export default {
 
       },
       formOptions: {
-
+        validateAfterLoad: false,
+        validateAfterChanged: true,
+        validateAsync: false,
       },
     }
   },
