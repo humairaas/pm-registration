399eb2eb38cbe9669e38b1be0e16eea9d40d3972
Kisyok <kisoindran@gmail.com>
2021-12-15 12:51:26 +0800

    delete vital

--- src/components/SHHARP-management/sh-list.vue
+++ src/components/SHHARP-management/sh-list.vue
@@ -134,7 +134,7 @@ export default {
         patientId: user.patient_id,
       }
       localStorage.setItem('ID', JSON.stringify(ID))
-      this.$router.push({ name: 'patient_consultation' })
+      this.$router.push({ name: 'patient-consultation' })
     },
     getTrendIcon (user) {
       if (user.trend === 'up') {
